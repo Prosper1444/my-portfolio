eb06a1bb8bec7a016949a4fbde47e1be0988339f
Prosper1444 <prosperosayinoma@gmail.com>
2026-02-14 14:04:41 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/* const inputEl = document.querySelector(".input");
+console.log(inputEl.checked);
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode"));
+const bodyEl = document.querySelector("body");
+
+updateBody();
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.background = "black";
+    bodyEl.style.color = "white";
+  } else {
+    bodyEl.style.background = linear - gradient(#eceffe, #ced6fb);
+    bodyEl.style.color = "black";
+  }
+}
+
+inputEl.addEventListener("input", () => {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
+ */
