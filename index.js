8904a6ee3715ea0734581239b608ed20ba1f1cf6
Prosper1444 <prosperosayinoma@gmail.com>
2026-02-14 20:11:03 +0100

    nav bar added

--- index.js
+++ index.js
@@ -24,3 +24,13 @@ function updateLocalStorage() {
   localStorage.setItem("mode", JSON.stringify(inputEl.checked));
 }
  */
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
